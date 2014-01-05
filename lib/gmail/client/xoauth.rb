15ed2dbd36c9ee65ef37b1e2e8d5d76e08fe11a0
npolavarapu <npolavarapu17@lawrenceville.org>
2014-01-05 16:09:32 -0500

    Update xoauth.rb

--- lib/gmail/client/xoauth.rb
+++ lib/gmail/client/xoauth.rb
@@ -28,6 +28,18 @@ module Gmail
         raise_errors and raise AuthorizationError, "Couldn't login to given GMail account: #{username}"        
       end
 
+      def access_token
+        consumer_options = {
+          :site               => "https://www.google.com",
+          :request_token_path => "/accounts/OAuthGetRequestToken",
+          :authorize_path     => "/accounts/OAuthAuthorizeToken",
+          :access_token_path  => "/accounts/OAuthGetAccessToken"
+        }
+        consumer = OAuth::Consumer.new(consumer_key, consumer_secret, consumer_options)
+        @access_token ||= OAuth::AccessToken.new(consumer, token, secret)
+        @access_token
+      end
+
       def smtp_settings
         [:smtp, {
            :address => GMAIL_SMTP_HOST,
@@ -38,14 +50,13 @@ module Gmail
              :consumer_key    => consumer_key,
              :consumer_secret => consumer_secret,
              :token           => token,
-             :token_secret    => secret
+             :token_secret    => secret,
+             :access_token    => access_token
            },
            :authentication => :xoauth,
            :enable_starttls_auto => true
          }]
       end
     end # XOAuth
-
-    register :xoauth, XOAuth
   end # Client
 end # Gmail
